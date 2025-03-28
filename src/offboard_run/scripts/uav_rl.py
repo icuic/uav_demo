709a184e136085cbf69d8938721e1deedcdecce3
Will <icuic@foxmail.com>
2025-03-28 14:14:14 +0800

    2m_4243 0327-0940

--- src/offboard_run/scripts/uav_rl.py
+++ src/offboard_run/scripts/uav_rl.py
@@ -117,8 +117,8 @@ if __name__ == "__main__":
     create_checkpoints_folder()
 
     algorithm = 'ddpg'
-    restore_from_checkpoint = False
-    restore_from = 0
+    restore_from_checkpoint = True
+    restore_from = 2870
     episode_from = 0
 
     env_name = 'UAVGymEnv/UAVLandingEnv-v0'
